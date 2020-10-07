9322ac45a2bd37ae2861704e531bf8037d8aa1c4
Arnas Bendaravičius <arnasbendaravicius@gmail.com>
2020-10-07 22:44:10 +0300

    Homework 2 finished push to repo

--- hwork/hw2/hw2_data.php
+++ hwork/hw2/hw2_data.php
@@ -14,9 +14,26 @@
         $weight = $_POST['weight'];
         $height = $_POST['height'] / 100;
 
-        $bmi = $weight / ($height * $height);
-        echo $bmi;
+        $bmi = $weight / ($height ** 2);
+        echo 'Your BMI is: ' . '<strong>' . round($bmi, 2) . '</strong>';
+        echo '<br>';
+        echo 'According to your BMI, you are ';
+
+        if ($bmi < 18.5) {
+            echo '<strong>Underweight</strong>.';
+        } elseif ($bmi >= 18.5 && $bmi < 24.9) {
+            echo 'of <strong>Normal Weight</strong>.';
+        } elseif ($bmi >= 25 && $bmi < 29.9) {
+            echo '<strong>Overweight</strong>.';
+        } elseif ($bmi >= 30) {
+            echo '<strong>Obese</strong>.';
+        } else {
+            echo '<strong>STUPID. Entering two values is not THAT HARD!</strong>';
+        }
         ?>
+        <br>
+        <br>
+        <a href="hw2.php"><button>Back to Calculator</button></a>
     </div>
 </body>
 
