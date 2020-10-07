b2c800b7926aadc2a827f16622a406e4e132efd0
Arnas Bendaravičius <arnasbendaravicius@gmail.com>
2020-10-07 22:27:12 +0300

    Homework 2 progress

--- hwork/hw2/hw2.php
+++ hwork/hw2/hw2.php
@@ -7,31 +7,12 @@
     <title>BMI Calculator</title>
 </head>
 <body>
-    <form action="hw2-2.php" method="POST">
+    <form action="hw2_data.php" method="POST">
         <label for="weight">Enter your weight (kg):</label>
         <input type="number" name="weight" id="weight" min="1"><br><br>
         <label for="height">Enter your height (cm):</label>
         <input type="number" name="height" id="height" min="1">
         <input type="submit" value="Calculate my BMI" id="submit">
-    </form>
-
-    <div class="output">
-        <?php
-            $weight = $_POST['weight'];
-            $height = $_POST['height'] / 100;
-
-            $bmi = $weight / ($height * $height);
-            echo $bmi;
-        ?>
-    </div>
-
-    <script>
-        button = document.getElementById('submit');
-        button.addEventListener("click", (e) => {
-            e.preventDefault();
-            document.querySelector('.output').style.display = 'block';
-        })
-    </script>
-    
+    </form>    
 </body>
 </html>
