2f5ba7a896ff3f9b2d29e854f54be3ea403c11ce
Arnas Bendaravičius <arnasbendaravicius@gmail.com>
2020-10-13 12:00:07 +0300

    Minor tweaks and updates

--- hwork/hw4/multiplication_table_generator/hw4-4-index.php
+++ hwork/hw4/multiplication_table_generator/hw4-4-index.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Multiplication table</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h1>Multiplication table generator</h1>
     <form action="./hw4-4.php" method="GET">
         <label for="number">Generate multiplication table of:</label>
-        <input type="number" name="number" id="number" placeholder="Enter your number">
+        <input type="number" min="1" name="number" id="number" placeholder="Enter your number">
         <input type="submit" value="Multiply!">
     </form>
 </body>
